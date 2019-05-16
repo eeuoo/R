44face9e90be5f05d64cdf276561a466d64598d9
eeuoo <lee512joo@gmail.com>
2019-05-17 00:09:17 +0900

    2019-05-17

--- adsp.R
+++ adsp.R
@@ -15,22 +15,25 @@ head(airquality)
 head(airquality, 3)
 
 
-??melt
-
+??melt   # melt는 reshape의 함수
 T = reshape::melt(airquality, id = c("month", "day"), na.rm = TRUE)
 
 
-reshape::cast(T, day~month~variable)
+??cast   # cast는 reshape의 함수
+reshape::cast(T, day~month~variable) # 행을 day, 열을 month로 변수 새롭게 배열
 
-??cast 
 
+# acast는 reshape의 함수
+# 각 변수들의 month 값 평균
+b = reshape2::acast(T, month~variable, mean)    
 
-b = reshape2::acast(T, month~variable, mean)
-
+# margin 관련 옵션, 행과 열에 대한 합계를 산출
 d = reshape::cast(T, month~variable, mean, margins = c("grand_row", "grand_col"))
 
+# subset 기능으로 특정 변수(ozone)만 처리
 e = reshape::cast(T, day~month, mean, subset = variable == "ozone")
 
+# range는 min은 _X1, max는 _X2를 변수 뒤에 붙여줌
 f = reshape::cast(T, month~variable, range)
 
 
