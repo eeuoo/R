ac9e7d3fe0e5806bf5de2d1362cb799ce6d1430c
eeuoo <lee512joo@gmail.com>
2019-05-16 23:53:14 +0900

    2019-05-16

--- adsp.R
+++ adsp.R
@@ -13,3 +13,19 @@ colnames(airquality) <- tolower( colnames(airquality) )
 
 head(airquality)
 head(airquality, 3)
+
+
+??melt
+
+T = reshape::melt(airquality, id = c("month", "day"), na.rm = TRUE)
+
+
+reshape::cast(T, day~month~variable)
+
+??cast
+
+b = reshape2::acast(T, month~variable, mean)
+
+b
+
+
