d92daeafe4c89f9a39816e2c5cb2d5d54c122f5f
eeuoo <lee512joo@gmail.com>
2020-07-07 22:52:53 +0900

    2020-07-07

---
+++ 01-tidy-text.R
@@ -0,0 +1,62 @@
+library(knitr)
+opts_chunk$set(message = FALSE, warning = FALSE, cache = TRUE)
+options(width = 100, dplyr.width = 100)
+library(ggplot2)
+theme_set(theme_light())
+
+
+text <- c("Because I could not stop for Death -",
+          "He kindly stopped for me -",
+          "The Carriage held but just Ourselves -",
+          "and Immortality")
+
+text
+
+library(dplyr)
+text_df <- data_frame(line = 1:4, text = text)
+text_df
+
+
+# library(remotes)
+# install_github("juliasilge/tidytext")
+library(tidytext)
+
+
+text_df %>%
+  unnest_tokens(word, text)
+
+library(janeaustenr)
+library(dplyr)
+library(stringr)
+
+original_books <- austen_books() %>%
+  group_by(book) %>%
+  mutate(linenumber = row_number(),
+         chapter = cumsum(str_detect(text, regex("^chapter [\\divxlc]", ignore_case = TRUE)))) %>%
+  ungroup()
+
+original_books
+
+tidy_books <- original_books %>%
+  unnest_tokens(word, text)
+
+tidy_books
+
+data("stop_words")
+
+tidy_books <- tidy_books %>% 
+  anti_join(stop_words)
+
+tidy_books %>%
+  count(word, sort = TRUE) 
+
+library(ggplot2)
+
+tidy_books %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 600) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
