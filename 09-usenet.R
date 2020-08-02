2c15a483a0fac921b49adb2d1a0c6e9daa41ad74
eeuoo <lee512joo@gmail.com>
2020-08-02 21:52:10 +0900

    2020-08-02

---
+++ 09-usenet.R
@@ -0,0 +1,55 @@
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(readr)
+
+
+training_folder <- "data/20news-bydate/20news-bydate-train/"
+
+read_folder <- function(infolder) {
+  tibble(file = dir(infolder, full.names = TRUE)) %>%
+    mutate(text = map(file, read_lines)) %>%
+    transmute(id = basename(file), text) %>%
+    unnest(text)
+}
+
+raw_text <- tibble(folder = dir(training_folder, full.names = TRUE)) %>%
+  mutate(folder_out = map(folder, read_folder)) %>%
+  unnest(cols = c(folder_out)) %>%
+  transmute(newsgroup = basename(folder), id, text)
+
+load("data/raw_text.rda")
+
+raw_text
+
+library(ggplot2)
+
+raw_text %>%
+  group_by(newsgroup) %>%
+  summarize(messages = n_distinct(id)) %>%
+  ggplot(aes(newsgroup, messages)) +
+  geom_col() +
+  coord_flip()
+
+
+library(stringr)
+
+cleaned_text <- raw_text %>%
+  group_by(newsgroup, id) %>%
+  filter(cumsum(text == "") > 0,
+         cumsum(str_detect(text, "^--")) == 0) %>%
+  ungroup()
+
+cleaned_text <- cleaned_text %>%
+  filter(str_detect(text, "^[^>]+[A-Za-z\\d]") | text == "",
+         !str_detect(text, "writes(:|\\.\\.\\.)$"),
+         !str_detect(text, "^In article <"),
+         !id %in% c(9704, 9985))
+
+library(tidytext)
+
+usenet_words <- cleaned_text %>%
+  unnest_tokens(word, text) %>%
+  filter(str_detect(word, "[a-z']$"),
+         !word %in% stop_words$word)
+
