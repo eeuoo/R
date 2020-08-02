cb523fa2c33401d1ecf345f3f789f928a985e7c3
eeuoo <lee512joo@gmail.com>
2020-08-02 21:53:32 +0900

    2020-08-02

--- 09-usenet.R
+++ 09-usenet.R
@@ -53,3 +53,67 @@ usenet_words <- cleaned_text %>%
   filter(str_detect(word, "[a-z']$"),
          !word %in% stop_words$word)
 
+
+usenet_words %>%
+  count(word, sort = TRUE)
+
+words_by_newsgroup <- usenet_words %>%
+  count(newsgroup, word, sort = TRUE) %>%
+  ungroup()
+
+words_by_newsgroup
+
+tf_idf <- words_by_newsgroup %>%
+  bind_tf_idf(word, newsgroup, n) %>%
+  arrange(desc(tf_idf))
+
+tf_idf
+
+tf_idf %>%
+  filter(str_detect(newsgroup, "^sci\\.")) %>%
+  group_by(newsgroup) %>%
+  top_n(12, tf_idf) %>%
+  ungroup() %>%
+  mutate(word = reorder(word, tf_idf)) %>%
+  ggplot(aes(word, tf_idf, fill = newsgroup)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~ newsgroup, scales = "free") +
+  ylab("tf-idf") +
+  coord_flip()
+
+plot_tf_idf <- function(d) {
+  d %>%
+    group_by(newsgroup) %>%
+    top_n(10, tf_idf) %>%
+    mutate(word = reorder(word, tf_idf)) %>%
+    ggplot(aes(word, tf_idf, fill = newsgroup)) +
+    geom_col(show.legend = FALSE) +
+    facet_wrap(~ newsgroup, scales = "free") +
+    ylab("tf-idf") +
+    coord_flip()
+}
+
+tf_idf %>%
+  filter(str_detect(newsgroup, "^rec\\.")) %>%
+  plot_tf_idf()
+
+library(widyr)
+
+newsgroup_cors <- words_by_newsgroup %>%
+  pairwise_cor(newsgroup, word, n, sort = TRUE)
+
+newsgroup_cors
+
+library(ggraph)
+library(igraph)
+set.seed(2017)
+
+newsgroup_cors %>%
+  filter(correlation > .4) %>%
+  graph_from_data_frame() %>%
+  ggraph(layout = "fr") +
+  geom_edge_link(aes(alpha = correlation, width = correlation)) +
+  geom_node_point(size = 6, color = "lightblue") +
+  geom_node_text(aes(label = name), repel = TRUE) +
+  theme_void()
+
