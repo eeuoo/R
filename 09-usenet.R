262a58b83eee8f5dd0552f2d24c72ca6d2c31858
eeuoo <lee512joo@gmail.com>
2020-08-03 20:44:00 +0900

    2020-08-02

--- 09-usenet.R
+++ 09-usenet.R
@@ -1,3 +1,6 @@
+# 사례 연구 : 유즈넷 텍스트 분석 
+
+### 전처리 
 library(dplyr)
 library(tidyr)
 library(purrr)
@@ -6,6 +9,7 @@ library(readr)
 
 training_folder <- "data/20news-bydate/20news-bydate-train/"
 
+# 폴더에서 모든 파일을 읽어 데이터 프레임에 넣는 함수
 read_folder <- function(infolder) {
   tibble(file = dir(infolder, full.names = TRUE)) %>%
     mutate(text = map(file, read_lines)) %>%
@@ -54,6 +58,8 @@ usenet_words <- cleaned_text %>%
          !word %in% stop_words$word)
 
 
+### 뉴스 그룹의 단어들 
+
 usenet_words %>%
   count(word, sort = TRUE)
 
@@ -63,6 +69,8 @@ words_by_newsgroup <- usenet_words %>%
 
 words_by_newsgroup
 
+
+### 뉴스 그룹 내에서 tf-idf 찾기
 tf_idf <- words_by_newsgroup %>%
   bind_tf_idf(word, newsgroup, n) %>%
   arrange(desc(tf_idf))
@@ -117,3 +125,145 @@ newsgroup_cors %>%
   geom_node_text(aes(label = name), repel = TRUE) +
   theme_void()
 
+
+### 토픽 모델링 
+# 최소한 50회 출현하는 단어들만 포함
+word_sci_newsgroups <- usenet_words %>%
+  filter(str_detect(newsgroup, "^sci")) %>%
+  group_by(word) %>%
+  mutate(word_total = n()) %>%
+  ungroup() %>%
+  filter(word_total > 50)
+
+# sci.crypt_14147과 같은 문서 이름을 사용해 문서-용어 행렬로 변환한다
+sci_dtm <- word_sci_newsgroups %>%
+  unite(document, newsgroup, id) %>%
+  count(document, word) %>%
+  cast_dtm(document, word, n)
+
+library(topicmodels)
+sci_lda <- LDA(sci_dtm, k = 4, control = list(seed = 2016))
+
+sci_lda %>%
+  tidy() %>%
+  group_by(topic) %>%
+  top_n(8, beta) %>%
+  ungroup() %>%
+  mutate(term = reorder_within(term, beta, topic)) %>%
+  ggplot(aes(term, beta, fill = factor(topic))) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~ topic, scales = "free_y") +
+  coord_flip() +
+  scale_x_reordered()
+
+sci_lda %>%
+  tidy(matrix = "gamma") %>%
+  separate(document, c("newsgroup", "id"), sep = "_") %>%
+  mutate(newsgroup = reorder(newsgroup, gamma * topic)) %>%
+  ggplot(aes(factor(topic), gamma)) +
+  geom_boxplot() +
+  facet_wrap(~ newsgroup) +
+  labs(x = "Topic",
+       y = "# of messages where this was the highest % topic")
+
+
+### 정서분석 
+newsgroup_sentiments <- words_by_newsgroup %>%
+  inner_join(get_sentiments("afinn"), by = "word") %>%
+  group_by(newsgroup) %>%
+  summarize(value = sum(value * n) / sum(n))
+
+newsgroup_sentiments %>%
+  mutate(newsgroup = reorder(newsgroup, value)) %>%
+  ggplot(aes(newsgroup, value, fill = value > 0)) +
+  geom_col(show.legend = FALSE) +
+  coord_flip() +
+  ylab("Average sentiment value")
+
+contributions <- usenet_words %>%
+  inner_join(get_sentiments("afinn"), by = "word") %>%
+  group_by(word) %>%
+  summarize(occurences = n(),
+            contribution = sum(value))
+
+contributions
+
+contributions %>%
+  top_n(25, abs(contribution)) %>%
+  mutate(word = reorder(word, contribution)) %>%
+  ggplot(aes(word, contribution, fill = contribution > 0)) +
+  geom_col(show.legend = FALSE) +
+  coord_flip()
+
+top_sentiment_words <- words_by_newsgroup %>%
+  inner_join(get_sentiments("afinn"), by = "word") %>%
+  mutate(contribution = value * n / sum(n))
+
+top_sentiment_words
+
+top_sentiment_words %>%
+  filter(str_detect(newsgroup, "^(talk|alt|misc)")) %>%
+  group_by(newsgroup) %>%
+  top_n(12, abs(contribution)) %>%
+  ungroup() %>%
+  mutate(newsgroup = reorder(newsgroup, contribution),
+         word = reorder(paste(word, newsgroup, sep = "__"), contribution)) %>%
+  ggplot(aes(word, contribution, fill = contribution > 0)) +
+  geom_col(show.legend = FALSE) +
+  scale_x_discrete(labels = function(x) gsub("__.+$", "", x)) +
+  facet_wrap(~ newsgroup, scales = "free") +
+  coord_flip() +
+  labs(x = "",
+       y = "Sentiment value * # of occurrences")
+
+sentiment_messages <- usenet_words %>%
+  inner_join(get_sentiments("afinn"), by = "word") %>%
+  group_by(newsgroup, id) %>%
+  summarize(sentiment = mean(value),
+            words = n()) %>%
+  ungroup() %>%
+  filter(words >= 5)
+
+sentiment_messages %>%
+  arrange(desc(sentiment))
+
+print_message <- function(group, message_id) {
+  result <- cleaned_text %>%
+    filter(newsgroup == group, id == message_id, text != "")
+  
+  cat(result$text, sep = "\n")
+}
+
+print_message("rec.sport.hockey", 53560)
+
+sentiment_messages %>%
+  arrange(sentiment)
+
+print_message("rec.sport.hockey", 53907)
+
+usenet_bigrams <- cleaned_text %>%
+  unnest_tokens(bigram, text, token = "ngrams", n = 2)
+
+usenet_bigram_counts <- usenet_bigrams %>%
+  count(newsgroup, bigram, sort = TRUE) %>%
+  separate(bigram, c("word1", "word2"), sep = " ")
+
+negate_words <- c("not", "without", "no", "can't", "don't", "won't")
+
+usenet_bigram_counts %>%
+  filter(word1 %in% negate_words) %>%
+  count(word1, word2, wt = n, sort = TRUE) %>%
+  inner_join(get_sentiments("afinn"), by = c(word2 = "word")) %>%
+  mutate(contribution = value * n) %>%
+  group_by(word1) %>%
+  top_n(10, abs(contribution)) %>%
+  ungroup() %>%
+  mutate(word2 = reorder(paste(word2, word1, sep = "__"), contribution)) %>%
+  ggplot(aes(word2, contribution, fill = contribution > 0)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~ word1, scales = "free", nrow = 3) +
+  scale_x_discrete(labels = function(x) gsub("__.+$", "", x)) +
+  xlab("Words preceded by a negation") +
+  ylab("Sentiment value * # of occurrences") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  coord_flip()
